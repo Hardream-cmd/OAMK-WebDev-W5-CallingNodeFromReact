ced2e32ee5c27a2ecef89bd845a83a67256293bf
Arnaud <arnaud.a.Durand@gmail.com>
2022-09-29 10:05:10 +0300

    backend wkg

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "node server is responding" });
+});
+
 app.listen(port, () => {
-  console.log("server is running");
+  console.log(`server is running ${port}`);
 });
